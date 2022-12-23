9edd2150cfe16d7110f07c81e6a2bb1843fd7732
Bharath <bharathg1252001@gmail.com>
2022-12-23 23:28:53 +0530

    completed

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-//your code here
-let item = document.getElementById('item-name-input');
-let price = document.getElementById('item-price-input');
-let add = document.getElementById('add');
+let itemInput = document.getElementById('item-name-input');
+let priceInput = document.getElementById('item-price-input');
+
+let button = document.getElementById('add');
+
 let table = document.getElementById('table');
+let total = document.getElementById('total');
 
-function addElement() {
-    let row = document.createElement('tr');
-    let data1 = document.createElement('td');
-    let data2 = document.createElement('td');
-   data1 = item.value;
-   data2 = price.value;
-    row.append(data1);
-    row.append(data2);
-    table.append(row);
+let priceArr = [];
+function addItem() {
+    let item = itemInput.value;
+    let price = priceInput.value;
+    let sum = 0;
 
-    console.log(data1);
-    console.log(data2);
+    if (item !== '' && price !== "") {
+        // you can start items to the screen
+        let row = document.createElement('tr');
+        priceArr.push(parseInt(price));
+        // console.log(priceArr);
+        row.innerHTML = `<td>${item} </td> <td>${price}</td> `
+        
+        for (let i = 0; i < priceArr.length; i++) {
+            sum += priceArr[i];
+        }
+        
+        console.log(sum);
+        table.append(row);
+        total.innerText = sum;
+        
+        itemInput.value = '';
+        priceInput.value = '';
+    }
 }
 
-add.addEventListener('click', addElement);
+button.addEventListener('click', addItem)
