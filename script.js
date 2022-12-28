9d1d8abaedaad9ec4f2837524d80aee93d2ca982
Bharath <bharathg1252001@gmail.com>
2022-12-28 20:42:29 +0530

    completed

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
-function insertItem() {
-  var name_elem=document.getElementById("item-name-input");
-  var name=name_elem.value;
-  var price_elem=document.getElementById("item-price-input");
-  var price=parseInt(price_elem.value);
+var itemName = document.getElementById("item-name-input");
+var itemPrice = document.getElementById("item-price-input");
 
-  name_elem.value='';
-  price_elem.value='';
+var tbody = document.querySelector("tbody");
 
-  if(name=='' || isNaN(price)){
-      return;
+var grandTotal = document.getElementById("total");
+var totalSoFar = 0;
+
+var addItem = document.querySelector("#add");
+
+addItem.addEventListener("click", (event) => {
+  // otherwise it will submit the form
+  event.preventDefault();
+  // invalid values
+  var nameValue = itemName.value.trim();
+  var priceValue = itemPrice.value;
+  if (nameValue.length == 0 || itemPrice <= 0) return;
+
+  let newRow = document.createElement("tr");
+
+  let name = document.createElement("td");
+  name.innerText = itemName.value;
+  name.setAttribute("data-ns-test", "item-name");
+
+  newRow.append(name);
+
+  let price = document.createElement("td");
+  price.innerText = itemPrice.value;
+  price.setAttribute("data-ns-test", "item-price");
+
+  newRow.append(price);
+
+  // is valid item
+  if (itemName.value && itemPrice.value) {
+    let total = document.createElement("td");
+    total.innerText = priceValue;
+
+    tbody.append(newRow);
+
+    totalSoFar += Number(total.innerText);
+    grandTotal.innerText = totalSoFar;
   }
-  
-  var newRow=document.createElement('tr');
-  var td1=document.createElement('td');
-  var td2=document.createElement('td');
-  var td3=document.createElement('td');
 
-  let sr_elem=document.querySelectorAll('[data-type=sr_no]');
-  let counter=sr_elem.length;
-
-  td1.innerText=counter+1; //todo fixed
-  td2.innerText=name;
-  td3.innerText=price;
-
-  td1.dataset.type="sr_no";
-  td2.dataset.nsTest="item-name";
-  td3.setAttribute("data-ns-test","item-price");
-
-  newRow.appendChild(td1);
-  newRow.appendChild(td2);
-  newRow.appendChild(td3);
-
-  let mainTable=document.getElementsByTagName('table')[0];
-  mainTable.appendChild(newRow);
-
-  let grandTotal=0;
-  let price_elements = document.querySelectorAll('[data-ns-test=item-price]');
-  price_elements.forEach((single_price_element) => {
-      let single_price=single_price_element.innerText;
-      single_price=parseInt(single_price);
-      grandTotal+=single_price;
-  });
-
-  document.querySelector('[data-ns-test=grandTotal]').innerText=grandTotal;
-  
-}
+  // reset
+  itemName.value = "";
+  itemPrice.value = "";
+});
